a43313cfcbc34da4d7d22cd6cad7a45a05f06a16
Marcel Rieger <marcelrieger@me.com>
2016-12-04 00:57:13 +0100

    Add parameters.

---
+++ law/parameter.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""
+Custom luigi parameters.
+"""
+
+__all__ = ["EMPTY_STR", "EMPTY_INT", "EMPTY_FLOAT"]
+
+
+EMPTY_STR = "EMPTY_STR"
+
+EMPTY_INT = -1
+
+EMPTY_FLOAT = -1.
