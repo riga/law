3bb6c239d20dbff761e8149e53c23dbc78a48053
Marcel Rieger <marcelrieger@me.com>
2016-12-06 18:47:02 +0100

    Update docker files.

--- docker/law-py34/entrypoint.sh
+++ docker/law-py34/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+python() { python3 "$@"; } && export -f python
+pip() { pip3 "$@"; } && export -f pip
+
 luigid --background &
 bash --init-file <(echo "source dev.sh; law_db")
