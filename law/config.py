e708614e58fcbf7643d05b6139a35494867ccd4c
Marcel Rieger <marcelrieger@me.com>
2016-12-07 09:37:45 +0100

    Imrove config interface, add config file inheritance, close #14.

--- law/config.py
+++ law/config.py
@@ -19,10 +19,42 @@ except ImportError:
     from ConfigParser import ConfigParser
 
 
-_no_default = object()
+class Config(ConfigParser):
 
+    _instance = None
 
-class Parser(ConfigParser):
+    _default_config = {
+        "core": {
+            "db_file": os.environ.get("LAW_DB_FILE", os.path.expandvars("$HOME/.law/db")),
+            "target_tmp_dir": tempfile.gettempdir(),
+        },
+        "paths": {},
+    }
+
+    _config_files = ("$LAW_CONFIG_FILE", "$HOME/.law/config", "etc/law/config")
+
+    @classmethod
+    def instance(cls, config_file=""):
+        if cls._instance is None:
+            cls._instance = cls(config_file=config_file)
+        return cls._instance
+
+    def __init__(self, config_file="", skip_fallbacks=False):
+        super(Config, self).__init__(allow_no_value=True)
+
+        files = (config_file,)
+        if not skip_fallbacks:
+            files += self._config_files
+
+        # read from files
+        self.read(os.path.expandvars(os.path.expanduser(f)) for f in files)
+
+        # maybe inherit
+        if self.has_section("core") and self.has_option("core", "inherit_config"):
+            self.inherit(self.get("core", "inherit_config"))
+
+        # update by defaults
+        self.update(self._default_config, overwrite=False)
 
     def optionxform(self, option):
         return option
@@ -35,53 +67,9 @@ class Parser(ConfigParser):
                 if overwrite or not self.has_option(section, option):
                     self.set(section, option, value)
 
-    def get_default(self, section, option, default=_no_default, cast=None):
-        if default != _no_default and not self.has_option(section, option):
-            return default
-        value = self.get(section, option)
-        if cast is not None:
-            value = cast(value)
-        return value
-
-
-class Config(object):
-
-    _instance = None
-
-    _defaults = {
-        "core": {
-            "db_file": os.environ.get("LAW_DB_FILE", os.path.expandvars("$HOME/.law/db")),
-            "target_tmp_dir": tempfile.gettempdir(),
-        },
-        "paths": {},
-    }
-
-    _config_file_order = ("$LAW_CONFIG_FILE", "$HOME/.law/config", "etc/law/config")
-
-    @classmethod
-    def instance(cls, config_file=None):
-        if cls._instance is None:
-            cls._instance = cls(config_file=config_file)
-        return cls._instance
-
-    def __init__(self, config_file=None):
-        super(Config, self).__init__()
-
-        for _config_file in (config_file or "",) + self._config_file_order:
-            _config_file = os.path.expandvars(os.path.expanduser(_config_file))
-            if os.path.isfile(_config_file):
-                config_file = _config_file
-                break
-
-        self._parser = Parser(allow_no_value=True)
-        self._parser.read(config_file)
-        self._parser.update(self._defaults, overwrite=False)
-
-    def get(self, section, option, default=_no_default, cast=None):
-        return self._parser.get_default(section, option, default=default, cast=None)
-
-    def items(self, section):
-        return self._parser.items(section)
+    def inherit(self, filename):
+        p = self.__class__(filename, skip_fallbacks=True)
+        self.update(p._sections, overwrite=False)
 
     def keys(self, section):
         return [key for key, _ in self.items(section)]
