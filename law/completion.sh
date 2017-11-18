9c35c7c7608d7cfa3b540b3672a22d2562164270
Marcel Rieger <marcel@mBook15.local>
2017-11-18 12:44:06 +0100

    Update completion.

--- law/completion.sh
+++ law/completion.sh
@@ -6,16 +6,7 @@ __law_complete() {
 		db_file="$HOME/.law/db"
 	fi
 
-	local cur="${COMP_WORDS[COMP_CWORD]}"
-
-	if [ "$COMP_CWORD" -gt "1" ] && [ "${COMP_WORDS[1]}" != "run" ]; then
-		return
-	fi
-
-	if [ "$COMP_CWORD" -gt "2" ]; then
-		local task_family="${COMP_WORDS[2]}"
-	fi
-
+	# cross-OS grep
 	_grep() {
 		if [ "$( uname -s )" != "Darwin" ]; then
 			grep -Po $@
@@ -24,45 +15,59 @@ __law_complete() {
 		fi
 	}
 
-	case $COMP_CWORD in
-		1)
-			COMPREPLY=( $( compgen -W "run db completion" "${cur}" ) )
-			;;
-		2)
+	local cur="${COMP_WORDS[COMP_CWORD]}"
+
+	# trivial case
+	if [ "$COMP_CWORD" = "0" ]; then
+		return
+	fi
+
+	# complete the subcommand
+	if [ "$COMP_CWORD" = "1" ]; then
+		COMPREPLY=( $( compgen -W "run db completion" "${cur}" ) )
+		return
+	fi
+
+	# complete run
+	if [ "${COMP_WORDS[1]}" = "run" ]; then
+		# task family
+		if [ "$COMP_CWORD" = "2" ]; then
 			COMPREPLY=( $( compgen -W "$( _grep "[^\:]+\:\K(.+)(?=\:.+)" "$db_file" )" "${cur}" ) )
-			;;
-		*)
-			local inp="${cur##-}"
-			inp="${inp##-}"
-			COMPREPLY=( $( compgen -W "$( _grep "[^\:]+\:$task_family\:\K.+" "$db_file" )" -P "--" -- "$inp" ) )
+			return
+		fi
+		local task_family="${COMP_WORDS[2]}"
 
-			if [ "${#COMPREPLY[@]}" = "0" ] && [ "${cur:0:2}" = "--" ]; then
-				local tasks=( $( _grep "[^\:]+\:\K$inp[^\:]*(?=\:.+)" "$db_file" ) )
+		# parameters
+		local inp="${cur##-}"
+		inp="${inp##-}"
+		COMPREPLY=( $( compgen -W "$( _grep "[^\:]+\:$task_family\:\K.+" "$db_file" )" -P "--" -- "$inp" ) )
 
-				if [[ "$( echo $inp | _grep "\K[^\.]+$" )" != *"-"* ]] && [ "${#tasks[@]}" -gt "1" ]; then
-					COMPREPLY=( $( compgen -W "$( echo ${tasks[@]} )" -P "--" -S "-" -- "$inp" ) )
-				else
-					local task="$( echo $inp | _grep ".*(?=\-[^\.]*)" )"
-					local curparam="$( echo $inp | _grep ".+\-\K[^\.]+$" )"
+		if [ "${#COMPREPLY[@]}" = "0" ] && [ "${cur:0:2}" = "--" ]; then
+			local tasks=( $( _grep "[^\:]+\:\K$inp[^\:]*(?=\:.+)" "$db_file" ) )
 
-					[[ "$( echo $inp | _grep "\K[^\.]+$" )" != *"-"* ]] && task="${tasks[0]}"
-					[ "$( echo ${task:${#task}-1} )" == "-" ] && task="${task:0:${#task}-1}"
+			if [[ "$( echo $inp | _grep "\K[^\.]+$" )" != *"-"* ]] && [ "${#tasks[@]}" -gt "1" ]; then
+				COMPREPLY=( $( compgen -W "$( echo ${tasks[@]} )" -P "--" -S "-" -- "$inp" ) )
+			else
+				local task="$( echo $inp | _grep ".*(?=\-[^\.]*)" )"
+				local curparam="$( echo $inp | _grep ".+\-\K[^\.]+$" )"
 
-					local params=( $( _grep "[^\:]\:$task\:\K.*" "$db_file" ) )
-					local words=()
-					for param in "${params[@]}"; do
-						if [ -z "$curparam" ] || [ ! -z "$( echo "$param" | _grep "^$curparam" )" ]; then
-							words+=("$task-$param")
-						fi
-					done
-					unset param
+				[[ "$( echo $inp | _grep "\K[^\.]+$" )" != *"-"* ]] && task="${tasks[0]}"
+				[ "$( echo ${task:${#task}-1} )" == "-" ] && task="${task:0:${#task}-1}"
 
-					COMPREPLY=( $( compgen -W "$( echo ${words[@]} )" -P "--" -- "$inp" ) )
-				fi
+				local params=( $( _grep "[^\:]\:$task\:\K.*" "$db_file" ) )
+				local words=()
+				for param in "${params[@]}"; do
+					if [ -z "$curparam" ] || [ ! -z "$( echo "$param" | _grep "^$curparam" )" ]; then
+						words+=("$task-$param")
+					fi
+				done
+				unset param
 
+				COMPREPLY=( $( compgen -W "$( echo ${words[@]} )" -P "--" -- "$inp" ) )
 			fi
-			;;
-	esac
+
+		fi
+	fi
 }
 export -f __law_complete
 
