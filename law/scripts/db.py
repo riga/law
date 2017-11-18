1197ddbd5610628432187c5f5a951cacae16cb3f
Marcel Rieger <marcel@mBook15.local>
2017-11-18 12:45:55 +0100

    Simplify db script, use plain modules instead of traversing paths.

--- law/scripts/db.py
+++ law/scripts/db.py
@@ -17,55 +17,44 @@ from law.config import Config
 from law.util import multi_match
 
 
+common_params = ["workers", "local-scheduler", "help", "log-level"]
+
+
 def setup_parser(sub_parsers):
     parser = sub_parsers.add_parser("db", prog="law db", description="law db file updater")
 
-    parser.add_argument("--default-namespace", "-n", help="a default task namespace to apply")
-    parser.add_argument("--python-path", "-p", action="store_true",
-                        help="also traverse in PYTHONPATH")
+    parser.add_argument("--modules", "-m", nargs="+", help="additional modules to traverse")
     parser.add_argument("--verbose", "-v", action="store_true", help="verbose output")
 
 
 def execute(args):
-    # determine paths to lookup
-    lookup = [p.strip() for p in Config.instance().keys("paths")]
-    if args.python_path and "PYTHONPATH" in os.environ:
-        lookup.extend(p.strip() for p in os.environ["PYTHONPATH"].split(os.pathsep))
+    # get modules to lookup
+    lookup = [m.strip() for m in Config.instance().keys("modules")]
+    if args.modules:
+        lookup += args.modules
 
-    # loop through paths, import everything to load tasks
-    for path in lookup:
-        path = str(os.path.expandvars(os.path.expanduser(path)))
-        if not path:
+    print("loading tasks from {} modules".format(len(lookup)))
+
+    # loop through modules, import everything to load tasks
+    for modid in lookup:
+        if not modid:
             continue
 
         if args.verbose:
-            print("looking into path '%s'" % path)
+            print("loading module '{}'".format(modid))
 
-        for base, dirnames, filenames in os.walk(path):
-            for filename in filenames:
-                filebase, fileext = os.path.splitext(filename)
-                if fileext != ".py" or filebase == "__main__":
-                    continue
-                fullpath = os.path.join(base, filename)
-                # try to find a matching path in sys.path to be able to import it
-                modparts = os.path.join(base, filebase).strip("/").split("/")
-                if modparts[-1] == "__init__":
-                    modparts.pop()
-                modid = ""
-                while modparts:
-                    modid = modparts.pop() + (modid and ".") + modid
-                    try:
-                        mod = import_module(modid)
+        try:
+            mod = import_module(modid)
+        except ImportError as e:
+            continue
 
-                        if args.verbose:
-                            print("    loaded module '%s'" % modid)
-                    except ImportError as e:
-                        pass
+        if args.verbose:
+            print("loaded module '{}'".format(modid))
 
     if args.verbose:
         print("")
 
-    # determine data to write: "module_id:task_family:param param ..."
+    # determine tasks to write into the db file
     seen_families = []
     task_classes = []
     lookup = [law.Task]
@@ -78,16 +67,19 @@ def execute(args):
         seen_families.append(cls.task_family)
 
         skip = cls.exclude_db or not six.callable(getattr(cls, "run", None)) \
-               or getattr(cls.run, "__isabstractmethod__", False)
+            or getattr(cls.run, "__isabstractmethod__", False)
         if not skip:
             task_classes.append(cls)
 
             if args.verbose:
-                print("added task class with family '%s'" % cls.task_family)
+                print("add task '{}'".format(cls.task_family))
+        else:
+            if args.verbose:
+                print("skip task '{}'".format(cls.task_family))
 
     def dbline(cls, default_namespace=None):
         # determine parameters
-        params = ["workers", "local-scheduler", "help"]
+        params = []
         for attr in dir(cls):
             member = getattr(cls, attr)
             if isinstance(member, luigi.Parameter):
@@ -95,13 +87,12 @@ def execute(args):
                 if not multi_match(attr, exclude, any):
                     params.append(attr.replace("_", "-"))
 
-        # use a truncated task family when a default namespace is set
-        if default_namespace is None:
-            family = cls.task_family
-        else:
-            family = cls.task_family[(len(default_namespace) + 1):]
+        # add common parameters
+        params += common_params
 
-        return cls.__module__ + ":" + family + ":" + " ".join(params)
+        # build and return the line
+        # format: "module_id:task_family:param param ..."
+        return "{}:{}:{}".format(cls.__module__, cls.task_family, " ".join(params))
 
     # write the db file
     dbfile = Config.instance().get("core", "db_file")
@@ -112,12 +103,4 @@ def execute(args):
         for cls in task_classes:
             f.write(dbline(cls) + "\n")
 
-        # include lines for default namespace
-        if args.default_namespace:
-            for cls in task_classes:
-                if cls.task_family.startswith(args.default_namespace + "."):
-                    f.write(dbline(cls, args.default_namespace) + "\n")
-
-            if args.verbose:
-                print("")
-                print("included tasks with default namespace '%s'" % args.default_namespace)
+    print("written {} task(s) to db".format(len(task_classes)))
