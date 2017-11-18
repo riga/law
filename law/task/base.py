152c2d5f3c42c0cc04e15740df1020339639d061
Marcel Rieger <marcel@mBook15.local>
2017-11-18 12:47:57 +0100

    Factor task base.

--- law/task/base.py
+++ law/task/base.py
@@ -11,7 +11,6 @@ __all__ = ["Task", "WrapperTask", "ProxyTask", "getreqs"]
 import os
 import sys
 import inspect
-import gc
 from socket import gethostname
 from collections import OrderedDict
 from abc import abstractmethod
@@ -21,21 +20,40 @@ import luigi.util
 import six
 
 import law
-from law.parameter import NO_STR, NO_INT, TaskInstanceParameter
+from law.parameter import NO_STR, NO_INT, TaskInstanceParameter, CSVParameter
 from law.util import colored, query_choice, multi_match
 
 
+class BaseRegister(luigi.task_register.Register):
+
+    def __new__(metacls, classname, bases, classdict):
+        # default attributes
+        classdict.setdefault("exclude_db", False)
+
+        # union "exclude_params_*" sets with those of all base classes
+        for base in bases:
+            for attr, base_params in vars(base).items():
+                if isinstance(base_params, set) and attr.startswith("exclude_params_"):
+                    params = classdict.setdefault(attr, set())
+                    params |= base_params
+
+        return super(BaseRegister, metacls).__new__(metacls, classname, bases, classdict)
+
+
+@six.add_metaclass(BaseRegister)
 class BaseTask(luigi.Task):
 
-    task_name = luigi.Parameter(default=NO_STR,
-        description="an optional task name, default: <className>")
-
     exclude_db = True
+    exclude_params_db = set()
+    exclude_params_req = set()
+    exclude_params_req_pass = set()
+    exclude_params_req_get = set()
 
-    exclude_params_db = {"task_name"}
-    exclude_params_req = {"task_name"}
-    exclude_params_req_receive = set()
-    exclude_params_req_transfer = set()
+    @staticmethod
+    def resource_name(name, host=None):
+        if host is None:
+            host = gethostname().partition(".")[0]
+        return "{}_{}".format(host, name)
 
     @classmethod
     def get_param_values(cls, *args, **kwargs):
@@ -63,10 +81,12 @@ class BaseTask(luigi.Task):
             _exclude = set(_exclude)
         elif not isinstance(_exclude, set):
             _exclude = {_exclude}
-        # also use this class' req and req_receive sets
-        # and the req and req_transfer sets of the instance's class
-        _exclude.update(cls.exclude_params_req, cls.exclude_params_req_receive)
-        _exclude.update(inst.exclude_params_req, inst.exclude_params_req_transfer)
+
+        # also use this class' req and req_get sets
+        # and the req and req_pass sets of the instance's class
+        _exclude.update(cls.exclude_params_req, cls.exclude_params_req_get)
+        _exclude.update(inst.exclude_params_req, inst.exclude_params_req_pass)
+
         # remove excluded parameters
         for name in list(params.keys()):
             if multi_match(name, _exclude, any):
@@ -77,28 +97,11 @@ class BaseTask(luigi.Task):
 
         return params
 
-    @staticmethod
-    def resource_name(name, host=None):
-        if host is None:
-            host = gethostname().partition(".")[0]
-        return "%s_%s" % (host, name)
-
-    def __init__(self, *args, **kwargs):
-        super(BaseTask, self).__init__(*args, **kwargs)
-
-        # set default task_name
-        if self.task_name == NO_STR:
-            self.task_name = self.__class__.__name__
-
-    def complete(self):
-        complete = super(BaseTask, self).complete()
-        gc.collect()
-        return complete
-
     def walk_deps(self, max_depth=-1, order="level"):
         # see https://en.wikipedia.org/wiki/Tree_traversal
         if order not in ("level", "pre"):
-            raise ValueError("unknown traversal order '%s', use 'level' or 'pre'" % order)
+            raise ValueError("unknown traversal order '{}', use 'level' or 'pre'".format(order))
+
         tasks = [(self, 0)]
         while len(tasks):
             task, depth = tasks.pop(0)
@@ -126,14 +129,14 @@ class BaseTask(luigi.Task):
                 continue
             raw = replace.get(name, getattr(self, name))
             val = param.serialize(raw)
-            arg = "--%s" % name.replace("_", "-")
+            arg = "--{}".format(name.replace("_", "-"))
             if isinstance(param, luigi.BoolParameter):
                 if raw:
                     args.append(arg)
             elif isinstance(param, (luigi.IntParameter, luigi.FloatParameter)):
-                args.extend([arg, "%s" % val])
+                args.extend([arg, str(val)])
             else:
-                args.extend([arg, "'%s'" % val])
+                args.extend([arg, "'{}'".format(val)])
 
         return args
 
@@ -142,53 +145,46 @@ class BaseTask(luigi.Task):
         pass
 
 
+class Register(BaseRegister):
+
+    def __call__(cls, *args, **kwargs):
+        inst = super(Register, cls).__call__(*args, **kwargs)
+
+        # check for interactive parameters
+        for param in inst.interactive_params:
+            value = getattr(inst, param)
+            if value:
+                try:
+                    getattr(inst, "_" + param)(*value)
+                except KeyboardInterrupt:
+                    print("\naborted")
+                law.util.abort("", exitcode=0)
+
+        return inst
+
+
+@six.add_metaclass(Register)
 class Task(BaseTask):
 
-    log = luigi.Parameter(default=NO_STR, significant=False,
-        description="a custom log file, default: <task.log_file>")
-    print_deps = luigi.IntParameter(default=NO_INT, significant=False,
-        description="print task dependencies, do not run any task, the passed number sets the "
-        "recursion depth where 0 means non-recursive, default: NO_INT")
-    print_status = luigi.IntParameter(default=NO_INT, significant=False,
-        description="print the task status, do not run any task, the passed numbers sets the "
-        "recursion depth where 0 means non-recursive, default: NO_INT")
-    purge_output = luigi.IntParameter(default=NO_INT, significant=False,
-        description="purge all outputs, do not run any task, the passed number sets the recursion "
-        "depth where 0 means non-recursive, default: NO_INT")
+    log = luigi.Parameter(default=NO_STR, significant=False, description="a custom log file, "
+        "default: <task.log_file>")
+    print_deps = CSVParameter(cls=luigi.IntParameter, default=[], significant=False,
+        description="print task dependencies, do not run any task, the passed numbers set the "
+        "recursion depth (0 means non-recursive)")
+    print_status = CSVParameter(cls=luigi.IntParameter, default=[], significant=False,
+        description="print the task status, do not run any task, the passed numbers set the "
+        "recursion depth (0 means non-recursive) and optionally the collection depth")
+    remove_output = CSVParameter(cls=luigi.IntParameter, default=[], significant=False,
+        description="remove all outputs, do not run any task, the passed number sets the recursion "
+        "depth (0 means non-recursive)")
 
-    exclude_db = False
+    interactive_params = ["print_deps", "print_status", "remove_output"]
 
-    exclude_params_req = BaseTask.exclude_params_req \
-        | {"print_deps", "print_status", "purge_output"}
+    exclude_db = True
+    exclude_params_req = set(interactive_params)
 
     def __init__(self, *args, **kwargs):
         super(Task, self).__init__(*args, **kwargs)
-
-        # check interactive parameters first
-        func = None
-        _kwargs = None
-
-        print_deps = kwargs.get("print_deps", NO_INT)
-        if print_deps != NO_INT:
-            func = self._print_deps
-            _kwargs = {"max_depth": print_deps}
-
-        print_status = kwargs.get("print_status", NO_INT)
-        if print_status != NO_INT:
-            func = self._print_status
-            _kwargs = {"max_depth": print_status}
-
-        purge_output = kwargs.get("purge_output", NO_INT)
-        if purge_output != NO_INT:
-            func = self._purge_output
-            _kwargs = {"max_depth": purge_output}
-
-        if func:
-            try:
-                func(**_kwargs)
-            except KeyboardInterrupt:
-                print("\naborted")
-            law.util.abort(exitcode=0)
 
         # cache for messages published to the scheduler
         self._message_cache = []
@@ -197,6 +193,20 @@ class Task(BaseTask):
     @property
     def log_file(self):
         return "-"
+
+    def publish_message(self, *args):
+        msg = " ".join(str(arg) for arg in args)
+
+        # simple print
+        print(msg)
+
+        # add to message cache and handle overflow
+        self._message_cache.append(msg)
+        if self._message_cache_size >= 0:
+            self._message_cache[:] = self._message_cache[-self._message_cache_size:]
+
+        # set status message using the current message cache
+        self.set_status_message("\n".join(self._message_cache))
 
     def colored_repr(self):
         params = self.get_params()
@@ -207,121 +217,37 @@ class Task(BaseTask):
         param_objs = dict(params)
         for param_name, param_value in param_values:
             if param_objs[param_name].significant:
-                s = colored(param_objs[param_name].serialize(param_value), "blue", style="bright")
-                sig_parts.append("%s=%s" % (param_name, s))
+                n = colored(param_name, "blue", style="bright")
+                v = param_objs[param_name].serialize(param_value)
+                sig_parts.append("{}={}".format(n, v))
 
-        task_str = "%s(%s)" % (colored(self.task_family, "green"), ", ".join(sig_parts))
+        task_str = "{}({})".format(colored(self.task_family, "green"), ", ".join(sig_parts))
 
         return task_str
 
-    def publish_message(self, *args):
-        msg = " ".join(str(arg) for arg in args)
+    def _print_deps(self, *args, **kwargs):
+        return print_task_deps(self, *args, **kwargs)
 
-        print(msg)
+    def _print_status(self, *args, **kwargs):
+        return print_task_status(self, *args, **kwargs)
 
-        self._message_cache.append(msg)
-        if self._message_cache_size >= 0:
-            self._message_cache[:] = self._message_cache[-self._message_cache_size:]
-
-        self.set_status_message("\n".join(self._message_cache))
-
-    def _print_deps(self, max_depth=0):
-        print("print deps with max_depth %s\n" % max_depth)
-        for task, _, depth in self.walk_deps(max_depth=max_depth, order="pre"):
-            print(depth * "|   " + "> " + task.colored_repr())
-        print("")
-
-    def _print_status(self, max_depth=0):
-        print("print status with max_depth %s\n" % max_depth)
-
-        col_depth = six.moves.input("target collection depth? [0*, int] ")
-        col_depth = 0 if col_depth == "" else int(col_depth)
-        print("")
-
-        done = []
-
-        for task, _, depth in self.walk_deps(max_depth=max_depth, order="pre"):
-            tpl = (depth * "|   ", task.colored_repr())
-            print("%s> check status of %s" % tpl)
-
-            if task in done:
-                print((depth + 1) * "|   " + "- " + colored("outputs already checked", "yellow"))
-            else:
-                done.append(task)
-
-                for outp in luigi.task.flatten(task.output()):
-                    tpl = ((depth + 1) * "|   ", outp.colored_repr())
-                    print("%s- check %s" % tpl)
-
-                    status_lines = outp.status_text(max_depth=col_depth).split("\n")
-                    status_text = status_lines[0]
-                    for line in status_lines[1:]:
-                        status_text += "\n" + (depth + 1) * "|   " + "     " + line
-                    tpl = ((depth + 1) * "|   ", status_text)
-                    print("%s  -> %s" % tpl)
-        print("")
-
-    def _purge_output(self, max_depth=0):
-        print("purge output with max_depth %s\n" % max_depth)
-
-        mode = query_choice("continue?", ("y", "n", "d", "i"), default="i")
-        if mode == "n":
-            return
-        elif mode == "d":
-            print("selected " + colored("dry mode", "blue", style="bright") + "\n")
-        elif mode == "i":
-            print("selected " + colored("interactive mode", "blue", style="bright") + "\n")
-        else:
-            print("")
-
-        done = []
-
-        for task, _, depth in self.walk_deps(max_depth=max_depth, order="pre"):
-            tpl = (depth * "|   ", task.colored_repr())
-            print("%s> remove output of %s" % tpl)
-
-            if mode == "i":
-                msg = tpl[0] + "  walk through outputs?"
-                task_mode = query_choice(msg, ("y", "n", "d"), default="y")
-                if task_mode == "n":
-                    continue
-
-            if task in done:
-                print((depth + 1) * "|   " + "- " + colored("outputs already removed", "yellow"))
-            else:
-                done.append(task)
-
-                for outp in luigi.task.flatten(task.output()):
-                    tpl = ((depth + 1) * "|   ", outp.colored_repr())
-                    print("%s- remove %s" % tpl)
-
-                    if mode == "d":
-                        continue
-
-                    if mode == "i" and task_mode != "d":
-                        msg = tpl[0] + "  remove?"
-                        if query_choice(msg, ("y", "n"), default="n") == "n":
-                            print(tpl[0] + "  skipped")
-                            continue
-
-                    outp.remove()
-
-                    print(tpl[0] + "  " + law.util.colored("removed", "red", style="bright"))
-        print("")
+    def _remove_output(self, *args, **kwargs):
+        return remove_task_output(self, *args, **kwargs)
 
 
 class WrapperTask(BaseTask):
 
-    exclude_db = False
-
     run = None
+
+    exclude_db = True
 
 
 class ProxyTask(BaseTask):
 
     task = TaskInstanceParameter()
 
-    exclude_params_req = BaseTask.exclude_params_req | {"task"}
+    exclude_db = True
+    exclude_params_req = {"task"}
 
 
 def getreqs(struct):
@@ -337,6 +263,88 @@ def getreqs(struct):
         try:
             s = list(struct)
         except TypeError:
-            raise Exception("Cannot map %s to Task/dict/list" % str(struct))
+            raise Exception("Cannot map {} to Task/dict/list".format(struct))
 
         return struct.__class__(getreqs(r) for r in s)
+
+
+def print_task_deps(task, max_depth=1):
+    print("print task dependencies with max_depth {}\n".format(max_depth))
+
+    ind = "|   "
+    for dep, _, depth in task.walk_deps(max_depth=max_depth, order="pre"):
+        print(depth * ind + "> " + dep.colored_repr())
+
+
+def print_task_status(task, max_depth=0, target_depth=0):
+    print("print task status with max_depth {} and target_depth {}\n".format(
+        max_depth, target_depth))
+
+    done = []
+    ind = "|   "
+    for dep, _, depth in task.walk_deps(max_depth=max_depth, order="pre"):
+        offset = depth * ind
+        print("{}> check status of {}".format(offset, dep.colored_repr()))
+        offset += ind
+
+        if dep in done:
+            print(offset + "- " + colored("outputs already checked", "yellow"))
+        else:
+            done.append(dep)
+
+            for outp in luigi.task.flatten(dep.output()):
+                print("{}- check {}".format(offset, outp.colored_repr()))
+
+                status_lines = outp.status_text(max_depth=target_depth).split("\n")
+                status_text = status_lines[0]
+                for line in status_lines[1:]:
+                    status_text += "\n" + offset + "     " + line
+                print("{}  -> {}".format(offset, status_text))
+
+
+def remove_task_output(task, max_depth=0, mode=None):
+    print("remove task output with max_depth {}\n".format(max_depth))
+
+    # determine the mode, i.e., all, dry, interactive
+    modes = ["i", "a", "d"]
+    mode_names = ["interactive", "all", "dry"]
+    if mode is None:
+        mode = query_choice("removal mode?", modes, default="i", descriptions=mode_names)
+    elif isinstance(mode, int):
+        mode = modes[mode]
+    else:
+        mode = mode[0].lower()
+    if mode not in modes:
+        raise Exception("unknown removal mode '{}'".format(mode))
+    mode_name = mode_names[modes.index(mode)]
+    print("selected " + colored(mode_name + " mode", "blue", style="bright") + "\n")
+
+    done = []
+    ind = "|   "
+    for dep, _, depth in task.walk_deps(max_depth=max_depth, order="pre"):
+        offset = depth * ind
+        print("{}> remove output of {}".format(offset, dep.colored_repr()))
+        offset += ind
+
+        if mode == "i":
+            task_mode = query_choice(offset + "  walk through outputs?", ("y", "n"), default="y")
+            if task_mode == "n":
+                continue
+
+        if dep in done:
+            print(offset + "- " + colored("outputs already removed", "yellow"))
+        else:
+            done.append(dep)
+
+            for outp in luigi.task.flatten(dep.output()):
+                print("{}- remove {}".format(offset, outp.colored_repr()))
+
+                if mode == "d":
+                    continue
+                elif mode == "i":
+                    if query_choice(offset + "  remove?", ("y", "n"), default="n") == "n":
+                        print(offset + colored("  skipped", "yellow"))
+                        continue
+
+                outp.remove()
+                print(offset + "  " + law.util.colored("removed", "red", style="bright"))
