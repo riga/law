a757fa961755973fbf149f6696772a1d9299455c
Marcel R <marcelrieger@me.com>
2017-11-26 19:16:30 +0100

    Add map_struct, mask_struct, tmp_file and interruptable_popen helpers.

--- law/util.py
+++ law/util.py
@@ -6,7 +6,7 @@ Helpful utility functions.
 
 
 __all__ = ["law_base", "printerr", "abort", "colored", "query_choice", "multi_match", "make_list",
-           "flatten", "which"]
+           "flatten", "which", "map_struct", "mask_struct", "tmp_file", "interruptable_popen"]
 
 
 import os
@@ -15,9 +15,19 @@ import types
 import re
 import fnmatch
 import tempfile
+import subprocess
+import signal
 from contextlib import contextmanager
 
 import six
+
+
+class _NoValue(object):
+
+    def __nonzero__(self):
+        return False
+
+_no_value = _NoValue()
 
 
 def law_base(*paths):
@@ -198,8 +208,134 @@ def which(prog):
                 return path
 
     return None
+
+
+def map_struct(func, struct, cls=None, map_dict=True, map_list=True, map_tuple=False,
+    map_set=False):
+    """
+    Applies a function *func* to each value of a complex structured object *struct* and returns the
+    output in the same structure. Example:
+
+    .. code-block:: python
+
+       struct = {"foo": [123, 456], "bar": [{"1": 1}, {"2": 2}]}
+       def times_two(i):
+           return i * 2
+
+       map_struct(struct, times_two)
+       # -> {"foo": [246, 912], "bar": [{"1": 2}, {"2": 4}]}
+
+    When *cls* is not *None*, it exclusively defines the class of objects that *func* is applied on.
+    All other objects are unchanged. *map_dict*, *map_list*, *map_tuple* and *map_set* configure if
+    objects of the respective types are traversed or mapped. The can be booleans or integer values
+    that define the depth of that setting in the struct.
+    """
+    types = tuple()
+    if map_dict:
+        types = types + (dict,)
+        if isinstance(map_dict, int) and not isinstance(map_dict, bool):
+            map_dict -= 1
+    if map_list:
+        types = types + (list,)
+        if isinstance(map_list, int) and not isinstance(map_list, bool):
+            map_list -= 1
+    if map_tuple:
+        types = types + (tuple,)
+        if isinstance(map_tuple, int) and not isinstance(map_tuple, bool):
+            map_tuple -= 1
+    if map_set:
+        types = types + (set,)
+        if isinstance(map_set, int) and not isinstance(map_set, bool):
+            map_set -= 1
+
+    # is an instance of cls?
+    if cls is not None and isinstance(struct, cls):
+        return func(struct)
+
+    # traverse?
+    elif isinstance(struct, types):
+        # create a new struct, treat tuples as lists for itertative item appending
+        new_struct = struct.__class__() if not isinstance(struct, tuple) else []
+
+        # create type-dependent generator and addition callback
+        if isinstance(struct, (list, tuple)):
+            gen = enumerate(struct)
+            add = lambda _, value: new_struct.append(value)
+        elif isinstance(struct, set):
+            gen = enumerate(struct)
+            add = lambda _, value: new_struct.add(value)
+        else: # dict
+            gen = six.iteritems(struct)
+            add = lambda key, value: new_struct.__setitem__(key, value)
+
+        # recursively fill the new struct
+        for key, value in gen:
+            value = map_struct(func, value, cls=cls, map_dict=map_dict, map_list=map_list,
+                map_tuple=map_tuple, map_set=map_set)
+            add(key, value)
+
+        # convert tuples
+        if isinstance(struct, tuple):
+            new_struct = struct.__class__(new_struct)
+
+        return new_struct
+
+    # when cls is set, just return
+    elif cls is not None:
+        return struct
+
+    # apply func
+    else:
+        return func(struct)
+
+
+def mask_struct(mask, struct, replace=_no_value):
+    # interpret genrators as lists
+    if isinstance(struct, types.GeneratorType):
+        return mask_struct(mask, list(struct), replace=replace)
+
+    # when mask is a bool, or struct is not a dict or sequence, apply the mask immediately
+    elif isinstance(mask, bool) or not isinstance(struct, (list, tuple, dict)):
+        return struct if mask else replace
+
+    # check list and tuple types
+    elif isinstance(struct, (list, tuple)) and isinstance(mask, (list, tuple)):
+        new_struct = []
+        for i, val in enumerate(struct):
+            if i >= len(mask):
+                new_struct.append(val)
+            else:
+                repl = _no_value
+                if isinstance(replace, (list, tuple)) and len(replace) > i:
+                    repl = replace[i]
+                val = mask_struct(mask[i], val, replace=repl)
+                if val != _no_value:
+                    new_struct.append(val)
+
+        return struct.__class__(new_struct) if new_struct else replace
+
+    # check dict types
+    elif isinstance(struct, dict) and isinstance(mask, dict):
+        new_struct = struct.__class__()
+        for key, val in six.iteritems(struct):
+            if key not in mask:
+                new_struct[key] = val
+            else:
+                repl = _no_value
+                if isinstance(replace, dict) and key in replace:
+                    repl = replace[key]
+                val = mask_struct(mask[key], val, replace=repl)
+                if val != _no_value:
+                    new_struct[key] = val
+        return new_struct or replace
+
+    # when this point is reached, mask and struct have incompatible types
+    raise TypeError("mask and struct must have the same type, got '{}' and '{}'".format(type(mask),
+        type(struct)))
+
+
 @contextmanager
-def tmpfile(*args, **kwargs):
+def tmp_file(*args, **kwargs):
     fileno, path = tempfile.mkstemp(*args, **kwargs)
 
     # create the file
@@ -212,3 +348,16 @@ def tmpfile(*args, **kwargs):
     finally:
         if os.path.exists(path):
             os.remove(path)
+
+
+def interruptable_popen(*args, **kwargs):
+    kwargs["preexec_fn"] = os.setsid
+    p = subprocess.Popen(*args, **kwargs)
+
+    try:
+        out, err = p.communicate()
+    except KeyboardInterrupt:
+        os.killpg(os.getpgid(p.pid), signal.SIGKILL)
+        raise
+
+    return p.returncode, out, err
