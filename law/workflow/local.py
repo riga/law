d9fd011a2750a01cac67aa6ca37c0aedc2a7ad94
Marcel R <marcelrieger@me.com>
2017-12-20 10:54:29 +0100

    Add missing run method to LocalWorkflow.

--- law/workflow/local.py
+++ law/workflow/local.py
@@ -15,10 +15,21 @@ class LocalWorkflowProxy(WorkflowProxy):
 
     workflow_type = "local"
 
+    def __init__(self, *args, **kwargs):
+        super(LocalWorkflowProxy, self).__init__(*args, **kwargs)
+
+        self._has_run = False
+
+    def complete(self):
+        return self._has_run
+
     def requires(self):
         reqs = super(LocalWorkflowProxy, self).requires()
         reqs["branches"] = self.task.get_branch_tasks()
         return reqs
+
+    def run(self):
+        self._has_run = True
 
 
 class LocalWorkflow(Workflow):
