7cc71d0f388339ecd189702e149237b641c8092e
Marcel R <marcelrieger@me.com>
2018-01-15 17:58:54 +0100

    Include all packages in setup script.

--- law/__init__.py
+++ law/__init__.py
@@ -12,7 +12,7 @@ __credits__ = ["Marcel Rieger"]
 __contact__ = "https://github.com/riga/law"
 __license__ = "MIT"
 __status__ = "Development"
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 
 __all__ = ["Task", "WrapperTask", "SandboxTask", "LocalWorkflow",
            "LocalFileSystem, LocalFileTarget, LocalDirectoryTarget",
--- setup.py
+++ setup.py
@@ -25,9 +25,7 @@ if os.path.isfile(readme) and "sdist" in sys.argv:
 else:
     long_description = ""
 
-keywords = [
-    "luigi", "workflow", "pipeline", "remote", "submission", "grid"
-]
+keywords = ["luigi", "workflow", "pipeline", "remote", "submission", "grid"]
 
 classifiers = [
     "Programming Language :: Python",
@@ -39,7 +37,7 @@ classifiers = [
     "Intended Audience :: Developers",
     "Intended Audience :: Science/Research",
     "Intended Audience :: Information Technology",
-    "Topic :: System :: Monitoring"
+    "Topic :: System :: Monitoring",
 ]
 
 install_requires = []
@@ -51,31 +49,43 @@ class install(_install):
     def run(self):
         _install.run(self) # old-style
 
-        with open(os.path.join(thisdir, "bin", "law")) as f:
+        with open(os.path.join(thisdir, "law", "scripts", "law")) as f:
             lines = f.readlines()
         with open(which("law"), "w") as f:
             f.write("".join(lines))
 
 setup(
-    name             = law.__name__,
-    version          = law.__version__,
-    author           = law.__author__,
-    author_email     = law.__email__,
-    description      = law.__doc__.strip(),
-    license          = law.__license__,
-    url              = law.__contact__,
-    keywords         = keywords,
-    classifiers      = classifiers,
+    name = law.__name__,
+    version = law.__version__,
+    author = law.__author__,
+    author_email = law.__email__,
+    description = law.__doc__.strip(),
+    license = law.__license__,
+    url = law.__contact__,
+    keywords = keywords,
+    classifiers = classifiers,
     long_description = long_description,
     install_requires = install_requires,
-    zip_safe         = False,
-    packages         = ["law", "law.task", "law.target", "law.sandbox", "law.workflow",
-                        "law.scripts", "law.examples"],
-    package_data     = {"": ["LICENSE", "requirements.txt", "README.md", "completion.sh"]},
-    cmdclass         = {"install": install},
-    entry_points     = {
-        "console_scripts": [
-            "law = law.scripts.cli:main"
-        ]
-    }
+    zip_safe = False,
+    packages = [
+        "law",
+        "law.task",
+        "law.target",
+        "law.sandbox",
+        "law.workflow",
+        "law.job",
+        "law.contrib.workflow",
+        "law.contrib.workflow.glite",
+        "law.contrib.job",
+        "law.contrib.util",
+        "law.scripts",
+        "law.examples",
+    ],
+    package_data = {
+        "": ["LICENSE", "requirements.txt", "README.md"],
+        "law": ["completion.sh"],
+        "law.contrib.workflow.glite": ["*.sh"],
+    },
+    cmdclass = {"install": install},
+    entry_points = {"console_scripts": ["law = law.scripts.cli:main"]},
 )
