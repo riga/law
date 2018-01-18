52143b8e0abd9bac2be5c8bbd9d705c6a095f78f
Marcel R <marcelrieger@me.com>
2018-01-18 19:00:41 +0100

    Version 0.0.6.

--- law/__init__.py
+++ law/__init__.py
@@ -12,7 +12,7 @@ __credits__ = ["Marcel Rieger"]
 __contact__ = "https://github.com/riga/law"
 __license__ = "MIT"
 __status__ = "Development"
-__version__ = "0.0.5"
+__version__ = "0.0.6"
 
 __all__ = ["Task", "WrapperTask", "SandboxTask", "LocalWorkflow",
            "LocalFileSystem, LocalFileTarget, LocalDirectoryTarget",
