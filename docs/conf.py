5b0ca5b827ea0ce67e8f6868f51413b252ad6bf5
Marcel R <marcelrieger@me.com>
2018-01-20 13:10:42 +0100

    [ci skip] Update docs config.

--- docs/conf.py
+++ docs/conf.py
@@ -4,38 +4,49 @@
 import sys
 import os
 
-sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import law
 
 
-project = "law"
+project = law.__name__
 author = law.__author__
 copyright = law.__copyright__
-version = law.__version__
+version = law.__version__[:law.__version__.index(".", 2)]
 release = law.__version__
-
 
 templates_path = ["_templates"]
 html_static_path = ["_static"]
 master_doc = "index"
 source_suffix = ".rst"
 
-
 exclude_patterns = []
 pygments_style = "sphinx"
-html_logo = "../logo.png"
-html_theme = "alabaster"
+html_title = project + " Documentation"
+html_logo = "../logo_docs.png"
 html_sidebars = {"**": [
     "about.html",
     "localtoc.html",
-    "searchbox.html"]
-}
-html_theme_options = {
-    "github_user": "riga",
-    "github_repo": "law",
-    "travis_button": True
-}
+    "searchbox.html",
+]}
 
-extensions = [
-    "sphinx.ext.autodoc"
-]
+html_theme = "sphinx_rtd_theme"
+html_theme_options = {}
+if html_theme == "sphinx_rtd_theme":
+    html_theme_options.update({
+        "logo_only": True,
+        "prev_next_buttons_location": None,
+        "collapse_navigation": False,
+    })
+elif html_theme == "alabaster":
+    html_theme_options.update({
+        "github_user": "riga",
+        "github_repo": "law",
+        "travis_button": True,
+    })
+
+extensions = ["sphinx.ext.autodoc"]
+
+
+def setup(app):
+    if html_theme in ("sphinx_rtd_theme", "alabaster"):
+        app.add_stylesheet("styles_{}.css".format(html_theme))
