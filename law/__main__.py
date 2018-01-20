1d55eb0160a8d76ac2e0957bdad6e9cd725b8be5
Marcel R <marcelrieger@me.com>
2018-01-20 15:58:20 +0100

    Add __main__.

---
+++ law/__main__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+if __name__ == "__main__":
+    # amend sys.path to ensure the law package relative to _this_ file is imported
+    import os
+    import sys
+
+    parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    sys.path.insert(0, parent_dir)
+
+    # load the law cli
+    from law.scripts.cli import main
+    main()
