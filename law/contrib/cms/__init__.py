4d5476911bccddd7cd989c2c0e26446f6fab9aba
Marcel R <marcelrieger@me.com>
2018-01-22 20:17:16 +0100

    Add cms contrib, start with CMSSW bundling task.

---
+++ law/contrib/cms/__init__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+"""
+CMS-related tasks and helpers.
+https://home.cern/about/experiments/cms
+"""
+
+
+__all__ = ["BundleCMSSW"]
+
+
+import os
+
+import luigi
+
+from law.task.base import Task
+from law.target.local import LocalFileTarget
+from law.parameter import CSVParameter
+from law.decorator import log
+from law.util import rel_path, interruptable_popen
+
+
+class BundleCMSSW(Task):
+
+    path = luigi.Parameter(description="the path to the CMSSW checkout to bundle")
+    exclude = CSVParameter(default=[], description="patterns of files to exclude")
+
+    def __init__(self, *args, **kwargs):
+        super(BundleGitRepository, self).__init__(*args, **kwargs)
+
+        self.path = os.path.expandvars(os.path.expanduser(os.path.abspath(self.path)))
+
+    def output(self):
+        return LocalFileTarget("{}.tgz".format(os.path.basename(self.path)))
+
+    @log
+    def run(self):
+        with self.output().localize("w") as tmp:
+            cmd = [rel_path(__file__, "bundle_cmssw.sh"), self.path, tmp.path]
+            if self.exclude:
+                cmd += [" ".join(self.exclude)]
+
+            code = interruptable_popen(cmd)[0]
+            if code != 0:
+                raise Exception("cmssw bundling failed")
---
+++ law/contrib/cms/bundle_cmssw.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# Bundles a local CMSSW checkout into a tar archive that is suitable for
+# unpacking on a machine with cvmfs.
+
+# Arguments:
+# 1. the path to the CMSSW checkout
+# 2. the path where the bundle should be stored, should end with .tgz
+# 3. (optional) space-separated list of files or directories to ignore, supports globbing
+
+action() {
+    local cmssw_path="$1"
+    if [ -z "$cmssw_path" ]; then
+        2>&1 echo "please provide the path to the CMSSW checkout to bundle"
+        return "1"
+    fi
+
+    if [ ! -d "$repo_path" ]; then
+        2>&1 echo "the provided path '$cmssw_path' is not a directory or does not exist"
+        return "2"
+    fi
+
+    local dst_path="$2"
+    if [ -z "$dst_path" ]; then
+        2>&1 echo "please provide the path where the bundle should be stored"
+        return "3"
+    fi
+
+    ( \
+        cd "$cmssw_path" && \
+        find src -maxdepth 3 -type d \
+            | grep -e "^src/.*/.*/\(interface\|data\|python\)" \
+            | tar -czf "$dst_path" lib biglib bin --exclude="*.pyc" --files-from -
+    )
+    local ret="$?"
+
+    return "$ret"
+}
+action "$@"
