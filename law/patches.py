0541ed0fb1250b46afa13a106534c2528116fa05
Marcel R <R>
2018-01-27 20:47:37 +0100

    Use different set of default luigi return codes.

--- law/patches.py
+++ law/patches.py
@@ -27,12 +27,26 @@ def patch_all():
         return
     _patched = True
 
+    patch_default_retcodes()
     patch_worker_run_task()
     patch_worker_factory()
     patch_keepalive_run()
     patch_cmdline_parser()
 
     logger.debug("applied law-specific luigi patches")
+
+
+def patch_default_retcodes():
+    import luigi.retcodes
+
+    retcode = luigi.retcodes.retcode
+
+    retcode.already_running._default = 10
+    retcode.missing_data._default = 20
+    retcode.not_run._default = 30
+    retcode.task_failed._default = 40
+    retcode.scheduling_error._default = 50
+    retcode.unhandled_exception._default = 60
 
 
 def patch_worker_run_task():
