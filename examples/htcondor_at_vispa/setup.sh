3c28683bc127099bff608ae612d29f32df4d18d2
Marcel R <marcelrieger@me.com>
2018-02-12 17:06:42 +0100

    Add luigi configs to examples.

--- examples/htcondor_at_vispa/setup.sh
+++ examples/htcondor_at_vispa/setup.sh
@@ -6,6 +6,7 @@ action() {
     export PYTHONPATH="$base:$PYTHONPATH"
     export LAW_HOME="$base/.law"
     export LAW_CONFIG_FILE="$base/law.cfg"
+    export LUIGI_CONFIG_PATH="$base/luigi.cfg"
 
     export ANALYSIS_PATH="$base"
     export ANALYSIS_DATA_PATH="$ANALYSIS_PATH/data"
