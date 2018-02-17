d0680f72857c15d85bb1d18583938269b63641b8
Marcel R <marcelrieger@me.com>
2018-02-17 15:39:50 +0100

    Refactor docker containers, close #30.

--- docker/law-py27/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-luigid --background &
-bash --init-file <(echo "source devenv.sh; law db")
--- docker/law-py34/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-python() { python3 "$@"; } && export -f python
-pip() { pip3 "$@"; } && export -f pip
-
-luigid --background &
-bash --init-file <(echo "source dev.sh; law db")
