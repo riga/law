d95fee52d2e30a2bf67905004885136df4c4d802
Marcel R <marcelrieger@me.com>
2018-03-14 18:36:16 +0100

    Version 0.0.21.

--- law/__init__.py
+++ law/__init__.py
@@ -14,7 +14,7 @@ __credits__ = ["Marcel Rieger"]
 __contact__ = "https://github.com/riga/law"
 __license__ = "MIT"
 __status__ = "Development"
-__version__ = "0.0.20"
+__version__ = "0.0.21"
 
 __all__ = [
     "Task", "WrapperTask", "SandboxTask",
