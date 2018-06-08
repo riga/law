889fa8ea0a79072e03126a84ab28f0dbebc8fdd3
Lukas Geiger <lukas.geiger94@gmail.com>
2018-06-08 11:56:26 +0200

    Add HDF5 Formatter
    This adds a HDF5 Formatter using [`h5py`](https://github.com/h5py/h5py). I added this functionality to `law.contrib.hdf5` in order to make it easy to add support for HDF5 using [`pytable](https://github.com/PyTables/PyTables) in the future.

---
+++ law/contrib/hdf5/formatter.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+"""
+HDF5 target formatters.
+"""
+
+from law.target.formatter import Formatter
+from law.target.file import get_path
+
+
+class H5pyFormatter(Formatter):
+
+    name = "h5py"
+
+    @classmethod
+    def accepts(cls, path):
+        path = get_path(path)
+        return path.endswith(".hdf5") or path.endswith(".h5")
+
+    @classmethod
+    def load(cls, path, *args, **kwargs):
+        import h5py
+
+        return h5py.File(get_path(path), 'r', *args, **kwargs)
+
+    @classmethod
+    def dump(cls, path, *args, **kwargs):
+        import h5py
+
+        return h5py.File(get_path(path), 'w', *args, **kwargs)
