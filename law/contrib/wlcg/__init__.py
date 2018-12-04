528e140eab8376a9b2dd4ae12557f9b55361a8a9
Marcel R <marcelrieger@me.com>
2018-12-04 18:55:26 +0100

    Expose missing wlcg contrib functions.

--- law/contrib/wlcg/__init__.py
+++ law/contrib/wlcg/__init__.py
@@ -8,14 +8,14 @@ Helpers and targets providing functionality to work with the Worldwide LHC Compu
 
 __all__ = [
     "WLCGFileSystem", "WLCGTarget", "WLCGFileTarget", "WLCGDirectoryTarget",
-    "get_voms_proxy_user", "get_voms_proxy_lifetime", "get_voms_proxy_vo", "renew_voms_proxy",
-    "delegate_voms_proxy_glite", "get_ce_endpoint"
+    "get_voms_proxy_file", "get_voms_proxy_user", "get_voms_proxy_lifetime", "get_voms_proxy_vo",
+    "check_voms_proxy_validity", "renew_voms_proxy", "delegate_voms_proxy_glite", "get_ce_endpoint",
 ]
 
 
 # provisioning imports
 from law.contrib.wlcg.target import WLCGFileSystem, WLCGTarget, WLCGFileTarget, WLCGDirectoryTarget
 from law.contrib.wlcg.util import (
-    get_voms_proxy_user, get_voms_proxy_lifetime, get_voms_proxy_vo, renew_voms_proxy,
-    delegate_voms_proxy_glite, get_ce_endpoint,
+    get_voms_proxy_file, get_voms_proxy_user, get_voms_proxy_lifetime, get_voms_proxy_vo,
+    check_voms_proxy_validity, renew_voms_proxy, delegate_voms_proxy_glite, get_ce_endpoint,
 )
