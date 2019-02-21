126f77373e50cf62199efc3dad75452f1945e3a1
Marcel R <marcelrieger@me.com>
2019-02-21 12:19:23 +0100

    Refactor gfal plugin setup script in cms contrib.

--- law/contrib/cms/scripts/setup_gfal_plugins.sh
+++ law/contrib/cms/scripts/setup_gfal_plugins.sh
@@ -8,7 +8,8 @@
 
 # Arguments:
 # 1. the absolute path to the new gfal plugin directory
-# 2. (optional) the path to the initial gfal plugin directory, defaults to $GFAL_PLUGIN_DIR
+# 2. (optional) the path to the initial gfal plugin directory, defaults to $GFAL_PLUGIN_DIR or, when
+#    empty, to the default plugin directory, obtained with _law_gfal2_default_plugin_dir
 
 action() {
     local dst_dir="$1"
@@ -19,8 +20,18 @@ action() {
 
     local src_dir="$2"
     if [ -z "$src_dir" ]; then
-        echo "using default the GFAL_PLUGIN_DIR at '$GFAL_PLUGIN_DIR' as source for plugins"
-        src_dir="$GFAL_PLUGIN_DIR"
+        echo "no plugin source directory passed"
+        if [ ! -z "$GFAL_PLUGIN_DIR" ]; then
+            echo "using GFAL_PLUGIN_DIR variable ($GFAL_PLUGIN_DIR)"
+            src_dir="$GFAL_PLUGIN_DIR"
+        else
+            src_dir="$( _law_gfal2_default_plugin_dir )"
+            if [ -z "$src_dir" ]; then
+                2>&1 echo "could not detect the default gfal2 plugin directory"
+                return "1"
+            fi
+            echo "detected the default gfal2 plugin directy ($src_dir)"
+        fi
     fi
 
     # check of the src_dir exists
@@ -32,22 +43,23 @@ action() {
     # create the dst_dir if required
     mkdir -p "$dst_dir"
     if [ "$?" != "0" ]; then
+        2>&1 echo "destination directory '$dst_dir' could not be created"
         return "1"
     fi
 
     # symlink all plugins
     ( \
         cd "$dst_dir" && \
-        ln -s $src_dir/libgfal_plugin_*.so . && \
-        rm -f libgfal_plugin_xrootd.so
+        ln -s $src_dir/libgfal_plugin_*.so .
     )
 
-    # download the precompiled xrootd plugin, use either wget or curl
+    # remove the xrootd plugin and download a version that was compiled ontop of CMSSW
+    rm -f "$dst_dir/libgfal_plugin_xrootd.so"
     local plugin_url="https://cernbox.cern.ch/index.php/s/qgrogVY4bwcuCXt/download"
     if [ ! -z "$( type curl 2> /dev/null )" ]; then
         ( \
             cd "$dst_dir" && \
-            curl "$plugin_url" > ibgfal_plugin_xrootd.so
+            curl "$plugin_url" > libgfal_plugin_xrootd.so
         )
     elif [ ! -z "$( type wget 2> /dev/null )" ]; then
         ( \
@@ -64,4 +76,32 @@ action() {
     export GFAL_PLUGIN_DIR="$dst_dir"
     echo "new GFAL_PLUGIN_DIR is '$GFAL_PLUGIN_DIR'"
 }
+
+_law_gfal2_default_plugin_dir() {
+    # there is no easy way to access the default gfal2 plugin dir within python
+    # in fact, the value is set through a preprocessor variable during gfal2 compilation and only
+    # used here: https://gitlab.cern.ch/dmc/gfal2/blob/a8a64e16427ec5a718bd77bcdbf80abe96de995e/src/core/common/gfal_plugin.c#L290
+    # although it is very dirty, one can parse the "[gfal_module_load]" log
+    local pycmd="\
+import os, logging, gfal2\n\
+try: from cStringIO import StringIO\n\
+except: from io import StringIO\n\
+s = StringIO()\n\
+logger = logging.getLogger('gfal2')\n\
+logger.addHandler(logging.StreamHandler(s))\n\
+logger.setLevel(logging.DEBUG)\n\
+gfal2.creat_context()\n\
+for line in s.getvalue().split('\\\\n'):\n\
+  line = line.strip()\n\
+  start = '[gfal_module_load] plugin '\n\
+  end = ' loaded with success'\n\
+  if line.startswith(start) and line.endswith(end):\n\
+    plugin = line[len(start):-len(end)].strip()\n\
+    plugin_dir = os.path.dirname(os.path.normpath(plugin))\n\
+    print(plugin_dir)\n\
+    break\n\
+"
+    GFAL_PLUGIN_DIR= echo -e "$pycmd" | python
+}
+
 action "$@"
