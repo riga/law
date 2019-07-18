f66b4f1621a044fb9a8381057a3c2c6b78db9419
Marcel R <marcelrieger@me.com>
2019-07-18 13:08:57 +0200

    [contrib/git] Commit changes in submodules before bundling.

--- law/contrib/git/scripts/bundle_repository.sh
+++ law/contrib/git/scripts/bundle_repository.sh
@@ -55,6 +55,8 @@ action() {
         sgit commit -m "[tmp] Add all changes." > /dev/null && \
         sgit archive --prefix="$repo_name/" --format=tar -o "$tmp_arc" HEAD && \
         sgit submodule foreach --recursive --quiet "\
+            sgit add -f . &> /dev/null && \
+            sgit commit -m \"[tmp] Add all changes.\" > /dev/null && \
             sgit archive --prefix=\"$repo_name/\$path/\" --format=tar --output=\"$rnd_\$sha1.tar\" HEAD && \
             tar --concatenate --file=\"$tmp_arc\" \"$rnd_\$sha1.tar\" \
             && rm \"$rnd_\$sha1.tar\"" && \
