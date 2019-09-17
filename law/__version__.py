28a4084d3655179b8d59c4ff012687cfd512c245
Marcel R <github.riga@icloud.com>
2019-09-17 16:21:37 +0200

    Bump version to 0.0.31.

--- law/__version__.py
+++ law/__version__.py
@@ -6,10 +6,10 @@ sandboxing.
 """
 
 __author__ = "Marcel Rieger"
-__email__ = "python-law@googlegroups.com"
+__email__ = "github.riga@icloud.com"
 __copyright__ = "Copyright 2018-2019, Marcel Rieger"
 __credits__ = ["Marcel Rieger"]
 __contact__ = "https://github.com/riga/law"
 __license__ = "BSD-3-Clause"
 __status__ = "Development"
-__version__ = "0.0.30"
+__version__ = "0.0.31"
