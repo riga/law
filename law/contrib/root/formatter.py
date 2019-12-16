11745d1ee9d9df31e0c5e6a0da71977bcfac4102
Marcel R <github.riga@icloud.com>
2019-12-16 19:10:04 +0100

    Refactor GuardedTFile in root contrib package.

--- law/contrib/root/formatter.py
+++ law/contrib/root/formatter.py
@@ -11,35 +11,46 @@ __all__ = ["GuardedTFile", "ROOTFormatter", "ROOTNumpyFormatter", "UprootFormatt
 from law.target.formatter import Formatter
 from law.target.file import get_path
 
+from law.contrib.root.util import import_ROOT
 
-guarded_tfile_cls = None
 
+class GuardedTFile(object):
 
-def GuardedTFile(*args, **kwargs):
-    """
-    Factory function that lazily creates the guarded TFile class, and creates and returns an
-    instance with all passed *args* and *kwargs*. This is required as we do not want to import ROOT
-    in the global scope.
-    """
-    global guarded_tfile_cls
+    @classmethod
+    def Open(cls, *args, **kwargs):
+        ROOT = import_ROOT()
+        return cls(ROOT.TFile.Open(*args, **kwargs))
 
-    if not guarded_tfile_cls:
-        import ROOT
-        ROOT.PyConfig.IgnoreCommandLineOptions = True
-        ROOT.gROOT.SetBatch()
+    def __init__(self, *args, **kwargs):
+        super(GuardedTFile, self).__init__()
 
-        class GuardedTFile(ROOT.TFile):
+        self._guarded_tfile = None
 
-            def __enter__(self):
-                return self
+        ROOT = import_ROOT()
+        if len(args) == 1 and isinstance(args[0], ROOT.TFile) and not kwargs:
+            self._guarded_tfile = args[0]
+        elif args or kwargs:
+            self._guarded_tfile = ROOT.TFile(*args, **kwargs)
 
-            def __exit__(self, exc_type, exc_value, traceback):
-                if self.IsOpen():
-                    self.Close()
+    def __enter__(self):
+        return self._guarded_tfile
 
-        guarded_tfile_cls = GuardedTFile
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self.IsOpen():
+            self.Close()
 
-    return guarded_tfile_cls(*args, **kwargs)
+    def __getattr__(self, attr):
+        if self._guarded_tfile is not None:
+            return getattr(self._guarded_tfile, attr)
+        else:
+            raise AttributeError("cannot forward attribute '{}' to undefined guarded tfile".format(
+                attr))
+
+    def __setattr__(self, attr, value):
+        if attr != "_guarded_tfile":
+            setattr(self._guarded_tfile, attr, value)
+        else:
+            super(GuardedTFile, self).__setattr__(attr, value)
 
 
 class ROOTFormatter(Formatter):
@@ -69,15 +80,14 @@ class ROOTNumpyFormatter(Formatter):
 
     @classmethod
     def load(cls, path, *args, **kwargs):
-        import ROOT
-        ROOT.PyConfig.IgnoreCommandLineOptions = True
-        ROOT.gROOT.SetBatch()
+        ROOT = import_ROOT()  # noqa: F841
         import root_numpy
 
         return root_numpy.root2array(get_path(path), *args, **kwargs)
 
     @classmethod
     def dump(cls, path, arr, *args, **kwargs):
+        ROOT = import_ROOT()  # noqa: F841
         import root_numpy
 
         return root_numpy.array2root(arr, get_path(path), *args, **kwargs)
