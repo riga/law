8eab91729a2eef0fc0857accd56ad78ccee4a531
Marcel R <github.riga@icloud.com>
2020-01-16 17:19:21 +0100

    Version 0.0.35.

--- law/contrib/htcondor/job.py
+++ law/contrib/htcondor/job.py
@@ -150,7 +150,6 @@ class HTCondorJobManager(BaseJobManager):
         # since v8.3.3 one can limit the number of jobs to query
         if self.htcondor_v833:
             cmd += ["-limit", str(len(job_ids))]
-
         # since v8.5.6 one can define the attributes to fetch
         if self.htcondor_v856:
             cmd += ["-attributes", ads]
