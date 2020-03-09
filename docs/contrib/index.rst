6c8ca77ff41c546e4a2369b219e3d2eda7181b0d
Peter Fackeldey <peter.fackeldey@rwth-aachen.de>
2020-03-09 10:56:55 +0100

    add coffea formatter (#94)
    * add coffea formatter
    
    * Add coffea docs.
    
    Co-authored-by: Marcel R <github.riga@icloud.com>

--- docs/contrib/index.rst
+++ docs/contrib/index.rst
@@ -20,6 +20,7 @@ To use on of the following packages in your code, you must import them explicitl
 
    arc
    cms
+   coffea
    docker
    dropbox
    git
---
+++ law/contrib/coffea/__init__.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+# flake8: noqa
+
+"""
+Coffea contrib functionality.
+"""
+
+
+__all__ = ["CoffeaFormatter"]
+
+
+# provisioning imports
+from law.contrib.coffea.formatter import CoffeaFormatter
