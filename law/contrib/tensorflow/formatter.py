70a92385bd5cb56d6f6d8a32fd0c715aa1f9e75e
Marcel R <github.riga@icloud.com>
2020-03-10 10:24:20 +0100

    Refactor tensorflow formatter.

--- law/contrib/tensorflow/formatter.py
+++ law/contrib/tensorflow/formatter.py
@@ -19,37 +19,109 @@ class TFConstantGraphFormatter(Formatter):
     name = "tf_const_graph"
 
     @classmethod
-    def accepts(cls, path):
-        return get_path(path).endswith(".pb")
-
-    @classmethod
-    def load(cls, path, graph=None, graph_def=None, as_text=False):
+    def import_tf(cls):
         import tensorflow as tf
 
-        if not graph:
-            graph = tf.Graph()
+        # keep a reference to the v1 API as long as v2 provides compatibility
+        tf1 = None
+        if tf.__version__.startswith("1."):
+            tf1 = tf
+        elif getattr(tf, "compat", None) and getattr(tf.compat, "v1"):
+            tf1 = tf.compat.v1
 
+        return tf, tf1
+
+    @classmethod
+    def accepts(cls, path, mode):
+        return get_path(path).endswith((".pb", ".pbtxt", ".pb.txt"))
+
+    @classmethod
+    def load(cls, path, create_session=None, as_text=None):
+        """
+        Reads a saved TensorFlow graph from *path* and returns it. When *create_session* is *True*,
+        a session object (compatible with the v1 API) is created and returned as the second value of
+        a 2-tuple. The default value of *create_session* is *True* when TensorFlow v1 is detected,
+        and *False* otherwise. When *as_text* is *True*, or *None* and the file extension is
+        ``".pbtxt"`` or ``".pb.txt"``, the content of the file at *path* is expected to be a
+        human-readable text file. Otherwise, it is expected to be a binary protobuf file. Example:
+
+        .. code-block:: python
+
+            graph = TFConstantGraphFormatter.load("path/to/model.pb", create_session=False)
+
+            graph, session = TFConstantGraphFormatter.load("path/to/model.pb", create_session=True)
+        """
+        tf, tf1 = cls.import_tf()
+        path = get_path(path)
+
+        # default create_session value
+        if create_session is None:
+            create_session = tf1 is not None
+
+        # default as_text value
+        if as_text is None:
+            as_text = path.endswith((".pbtxt", ".pb.txt"))
+
+        graph = tf.Graph()
         with graph.as_default():
-            if not graph_def:
-                graph_def = tf.GraphDef()
+            graph_def = graph.as_graph_def()
 
             if as_text:
+                # use a simple pb reader to load the file into graph_def
                 from google.protobuf import text_format
-                with open(get_path(path), "r") as f:
+                with open(path, "r") as f:
                     text_format.Merge(f.read(), graph_def)
-            else:
-                from tensorflow.python.platform import gfile
-                with gfile.FastGFile(get_path(path), "rb") as f:
-                    graph_def.ParseFromString(f.read())
 
-        return graph_def
+            else:
+                # use the gfile api depending on the TF version
+                if tf1:
+                    from tensorflow.python.platform import gfile
+                    with gfile.FastGFile(path, "rb") as f:
+                        graph_def.ParseFromString(f.read())
+                else:
+                    with tf.io.gfile.GFile(path, "rb") as f:
+                        graph_def.ParseFromString(f.read())
+
+            # import the graph_def (pb object) into the actual graph
+            tf.import_graph_def(graph_def, name="")
+
+        if create_session:
+            if not tf1:
+                raise NotImplementedError("the v1 compatibility layer of TensorFlow v2 is missing, "
+                    "but required by when create_session is True")
+            session = tf1.Session(graph=graph)
+            return graph, session
+        else:
+            return graph
 
     @classmethod
     def dump(cls, path, session, output_names, *args, **kwargs):
-        import tensorflow as tf
+        """
+        Takes a TensorFlow *session* object (compatible with the v1 API), converts its contained
+        graph into a simpler version with variables translated into constant tensors, and saves it
+        to a protobuf file at *path*. *output_numes* must be a list of names of output tensors to
+        save. In turn, TensorFlow internally determines which subgraph(s) to convert and save. All
+        *args* and *kwargs* are forwarded to :py:func:`tf.compat.v1.train.write_graph`.
 
+        .. note::
+
+            When used with TensorFlow v2, this function requires the v1 API compatibility layer.
+            When :py:attr:`tf.compat.v1` is not available, a *NotImplementedError* is raised.
+        """
+        _, tf1 = cls.import_tf()
+
+        # complain when the v1 compatibility layer is not existing
+        if not tf1:
+            raise NotImplementedError("the v1 compatibility layer of TensorFlow v2 is missing, but "
+                "required")
+
+        # convert the graph
+        constant_graph = tf1.graph_util.convert_variables_to_constants(session,
+            session.graph.as_graph_def(), output_names)
+
+        # default as_text value
+        kwargs.setdefault("as_text", path.endswith((".pbtxt", ".pb.txt")))
+
+        # write the graph
         graph_dir, graph_name = os.path.split(get_path(path))
-
-        const_graph = tf.graph_util.convert_variables_to_constants(
-            session, session.graph.as_graph_def(), output_names)
-        tf.train.write_graph(const_graph, graph_dir, graph_name, *args, **kwargs)
+        return tf1.train.write_graph(constant_graph, graph_dir, graph_name, *args, **kwargs)
