8fcf780bf45652f45ac8fd7f4621d7557b6ddaca
Marcel R <github.riga@icloud.com>
2020-07-27 12:43:40 +0200

    Add test scripts.

---
+++ tests/run.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# Script to run all tests.
+# Arguments:
+#   1. The test command. Defaults to "python3 -m unittest tests" when Python 3 is detected, and to
+#      "python -m unittest tests" otherwise.
+
+action() {
+    local this_file="$( [ ! -z "$ZSH_VERSION" ] && echo "${(%):-%x}" || echo "${BASH_SOURCE[0]}" )"
+    local this_dir="$( cd "$( dirname "$this_file" )" && pwd )"
+    local repo_dir="$( cd "$( dirname "$this_dir" )" && pwd )"
+
+    # detect python
+    local python_exe="python"
+    if [ "$LAW_IMAGE_PYTHON_MAJOR" = "3" ]; then
+        python_exe="python3"
+    elif [ "$LAW_IMAGE_PYTHON_MAJOR" != "2" ] && type python3 &> /dev/null; then
+        python_exe="python3"
+    fi
+
+    # default test command
+    local cmd="${1:-$python_exe -m unittest tests}"
+
+    # execute it
+    echo "command: $cmd"
+    (
+        cd "$repo_dir"
+        eval "$cmd"
+    )
+}
+action "$@"
