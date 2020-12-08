bf7b71bdee7c9bf68cef9577c48e3f6d43c9ccb2
Marcel R <github.riga@icloud.com>
2020-12-08 21:54:32 +0100

    Use boost 1.74 again in docker images for the moment.

--- setup.py
+++ setup.py
@@ -87,6 +87,7 @@ setup(
     keywords=" ".join(keywords),
     classifiers=classifiers,
     long_description=long_description,
+    long_description_content_type="text/x-rst",
     install_requires=install_requires,
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, <4'",
     extras_require={
