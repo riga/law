60686e1ed95e616b314337b742de5e53109135a1
Marcel R <github.riga@icloud.com>
2022-12-30 16:33:10 +0100

    Version 0.1.11.

--- law/__version__.py
+++ law/__version__.py
@@ -7,9 +7,9 @@ sandboxing.
 
 __author__ = "Marcel Rieger"
 __email__ = "github.riga@icloud.com"
-__copyright__ = "Copyright 2018-2022, Marcel Rieger"
+__copyright__ = "Copyright 2018-2023, Marcel Rieger"
 __credits__ = ["Marcel Rieger"]
 __contact__ = "https://github.com/riga/law"
 __license__ = "BSD-3-Clause"
 __status__ = "Development"
-__version__ = "0.1.10"
+__version__ = "0.1.11"
