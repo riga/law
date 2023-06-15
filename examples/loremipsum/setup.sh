e470fd3b911632c1e0762cebbc4ee811372576d5
Marcel R <github.riga@icloud.com>
2023-06-15 15:18:45 +0200

    Fix entry point in example image, close #154.

--- examples/loremipsum/setup.sh
+++ examples/loremipsum/setup.sh
@@ -5,19 +5,22 @@ action() {
     local this_file="$( ${shell_is_zsh} && echo "${(%):-%x}" || echo "${BASH_SOURCE[0]}" )"
     local this_dir="$( cd "$( dirname "${this_file}" )" && pwd )"
 
-    # setup external software once
-    local sw_dir="${this_dir}/tmp"
-    if [ ! -d "${sw_dir}" ]; then
-        mkdir -p "${sw_dir}"
-        git clone https://github.com/spotify/luigi.git "${sw_dir}/luigi"
-        ( cd "${sw_dir}/luigi" && git checkout tags/2.8.13 )
-        git clone https://github.com/benjaminp/six.git "${sw_dir}/six"
+    # setup external software once when not in the example image
+    if [ -z "${LAW_DOCKER_EXAMPLE}" ]; then
+        local sw_dir="${this_dir}/tmp"
+        if [ ! -d "${sw_dir}" ]; then
+            mkdir -p "${sw_dir}"
+            git clone https://github.com/spotify/luigi.git "${sw_dir}/luigi"
+            ( cd "${sw_dir}/luigi" && git checkout tags/2.8.13 )
+            git clone https://github.com/benjaminp/six.git "${sw_dir}/six"
+        fi
+
+        # adjust paths to find local software
+        export PATH="${law_base}/bin:${sw_dir}/luigi/bin:${PATH}"
+        export PYTHONPATH="${law_base}:${sw_dir}/luigi:${sw_dir}/six:${PYTHONPATH}"
     fi
 
-    local law_base="$( dirname "$( dirname "${this_dir}" )" )"
-    export PATH="${law_base}/bin:${sw_dir}/luigi/bin:${PATH}"
-    export PYTHONPATH="${this_dir}:${law_base}:${sw_dir}/luigi:${sw_dir}/six:${PYTHONPATH}"
-
+    export PYTHONPATH="${this_dir}:${PYTHONPATH}"
     export LAW_HOME="${this_dir}/.law"
     export LAW_CONFIG_FILE="${this_dir}/law.cfg"
     export DATA_PATH="${this_dir}/data"
