93574bd709a866e1344ba36931fd887a035dda3f
Marcel R. <github.riga@icloud.com>
2024-07-18 09:47:13 +0200

    Allow job_file_dir_mkdtmp to be path patterns.

--- law/contrib/arc/workflow.py
+++ law/contrib/arc/workflow.py
@@ -11,6 +11,9 @@ import os
 from abc import abstractmethod
 from collections import OrderedDict
 
+import six
+
+from law.config import Config
 from law.workflow.remote import BaseRemoteWorkflow, BaseRemoteWorkflowProxy
 from law.job.base import JobArguments, JobInputFile, DeprecatedInputFiles
 from law.task.proxy import ProxyCommand
@@ -211,9 +214,26 @@ class ARCWorkflow(BaseRemoteWorkflow):
         return ARCJobFileFactory
 
     def arc_create_job_file_factory(self, **kwargs):
+        # get the file factory cls
+        factory_cls = self.arc_job_file_factory_cls()
+
         # job file fectory config priority: kwargs > class defaults
         kwargs = merge_dicts({}, self.arc_job_file_factory_defaults, kwargs)
-        return self.arc_job_file_factory_cls()(**kwargs)
+
+        # default mkdtemp value which might require task-level info
+        if kwargs.get("mkdtemp") is None:
+            cfg = Config.instance()
+            mkdtemp = cfg.get_expanded(
+                "job",
+                cfg.find_option("job", "arc_job_file_dir_mkdtemp", "job_file_dir_mkdtemp"),
+            )
+            if isinstance(mkdtemp, six.string_types) and mkdtemp.lower() not in {"true", "false"}:
+                kwargs["mkdtemp"] = factory_cls._expand_template_path(
+                    mkdtemp,
+                    variables={"task_id": self.live_task_id, "task_family": self.task_family},
+                )
+
+        return factory_cls(**kwargs)
 
     def arc_job_config(self, config, job_num, branches):
         return config
