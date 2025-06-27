bbeaa4ad3213c9a01417061fd4c262947849e952
Marcel R. <github.riga@icloud.com>
2025-06-27 17:30:03 +0200

    Add python_exe render variable to adjust python in jobs.

--- law/job/law_job.sh
+++ law/job/law_job.sh
@@ -47,6 +47,7 @@
 # - stageout_command: A command that is executed after running tasks.
 # - stageout_file: A file that is executed after running tasks.
 # - log_file: A file for logging stdout and stderr simultaneously.
+# - python_exe: Python executable to be used for various setup steps. If not set, a default is used.
 #
 # Dashboard hooks (called when found in environment):
 # - law_hook_job_running: A function that is called right before the job setup starts. No arguments.
@@ -120,6 +121,7 @@ law_job() {
     local input_files_render
     input_files_render=( {{input_files_render}} )
     local render_variables="{{render_variables}}"
+    local python_exe="{{python_exe}}"
 
     mkdir -p "${LAW_JOB_HOME}"
     mkdir -p "${LAW_JOB_TMP}"
@@ -134,8 +136,12 @@ law_job() {
     }
 
     _law_python() {
-        # forward to python if it exists, otherwise to python3
-        _law_exe_exists python && python "$@" || python3 "$@"
+        # use python_exe if set, otherwise forward to python if it exists, otherwise to python3
+        if [ ! -z "${python_exe}" ]; then
+            "${python_exe}" "$@"
+        else
+            _law_exe_exists python && python "$@" || python3 "$@"
+        fi
     }
 
     _law_job_line() {
@@ -404,8 +410,8 @@ law_job() {
     _law_job_subsection "job infos"
     echo "shell    : ${SHELL}"
     echo "hostname : $( hostname )"
-    echo "python   : $( _law_exe_exists python && echo "$( 2>&1 python --version ) from $( which python )" || echo "missing"  )"
-    echo "python3  : $( _law_exe_exists python3 && echo "$( 2>&1 python3 --version ) from $( which python3 )" || echo "missing"  )"
+    echo "python   : $( _law_exe_exists python && echo "$( 2>&1 python --version ) from $( which python )" || echo "missing" )"
+    echo "python3  : $( _law_exe_exists python3 && echo "$( 2>&1 python3 --version ) from $( which python3 )" || echo "missing" )"
     echo "init dir : ${LAW_JOB_INIT_DIR}"
     echo "job home : ${LAW_JOB_HOME}"
     echo "tmp dir  : $( _law_python -c "from tempfile import gettempdir; print(gettempdir())" )"
